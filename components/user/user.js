c839ad1414e60fe03a95c2813ee074881e7406d0
cesine <cesine@yahoo.com>
2016-07-03 17:57:57 -0400

    added user fetch

--- components/user/user.js
+++ components/user/user.js
@@ -1,5 +1,7 @@
-(function(exports) {
+(function(exports, global) {
   'use strict';
+
+  global.fetch = global.fetch || require('node-fetch');
 
   /**
    * @class The User Model knows how to handle a
@@ -12,8 +14,48 @@
     /**
      * @param {Object} json Serialized json from a previuos app load
      */
-    constructor() {}
+    constructor(json) {
+      console.log('Constructing user', json);
+
+      if (json) {
+        for (const attr in json) {
+          if (json.hasOwnProperty(attr)) {
+            this[attr] = json[attr];
+          }
+        }
+      }
+    }
+
+    get url() {
+      return 'http://localhost:3000/v1/users/' + this.username;
+    }
+
+    fetch() {
+      const self = this;
+      let xhr;
+
+      return global.fetch(this.url).then(function(response) {
+        xhr = response;
+        return response.json();
+      }).then(function(data) {
+        if (xhr.status >= 400) {
+          data.status = xhr.status;
+          data.statusText = xhr.statusText;
+          data.url = xhr.url;
+          throw data;
+        }
+
+        if (data) {
+          for (const attr in data) {
+            if (data.hasOwnProperty(attr)) {
+              self[attr] = data[attr];
+            }
+          }
+          return self;
+        }
+      });
+    }
   }
 
   exports.User = User;
-})(exports);
+})(exports, global);
--- test/components/user/user-spec.js
+++ test/components/user/user-spec.js
@@ -1,9 +1,113 @@
 'use strict';
+/* globals fetchMock */
+
+let fetchMockLocal;
+
+try {
+  fetchMockLocal = fetchMock;
+} catch (err) {
+  fetchMockLocal = require('fetch-mock');
+}
 
 const User = exports.User || require('./../../../components/user/user').User;
 
-describe('User', function(){
-  it('should load', function(){
+describe('User', function() {
+  it('should load', function() {
     expect(User).toBeDefined();
+  });
+
+  describe('contruction', function() {
+    it('should construct', function() {
+      var user = new User();
+      expect(user).toBeDefined();
+    });
+
+    it('should support json', function() {
+      var user = new User({
+        username: 'anonymouse'
+      });
+
+      expect(user).toBeDefined();
+      expect(user.username).toEqual('anonymouse');
+      expect(user.url).toEqual('http://localhost:3000/v1/users/anonymouse');
+    });
+  });
+
+  describe('profile', function() {
+    afterEach(function() {
+      fetchMockLocal.restore();
+    });
+
+    it('should fetch', function(done) {
+      var user = new User({
+        username: 'anonymouse'
+      });
+
+      fetchMockLocal.mock({
+        name: 'anonymouse_json',
+        matcher: /\/v1\/users\/anonymouse/,
+        method: 'GET',
+        response: {
+          id: 1,
+          givenName: 'Anony',
+          language: 'fr',
+          username: 'anonymouse',
+          createdAt: '2016-05-15T15:04:18.027Z',
+          updatedAt: '2016-05-15T15:04:18.027Z'
+        }
+      });
+
+      user.fetch()
+        .then(function(result) {
+          expect(result).toBe(user);
+          expect(user.givenName).toEqual('Anony');
+          expect(user.language).toEqual('fr');
+          expect(user.createdAt).toEqual('2016-05-15T15:04:18.027Z');
+
+          expect(fetchMockLocal.called('anonymouse_json')).toBeTruthy();
+          done();
+        })
+        .catch(function(err) {
+          expect(err.message).toEqual('Failed to fetch');
+
+          expect(fetchMockLocal.called('anonymouse_json')).toBeTruthy();
+          done();
+        });
+    });
+
+    it('should handle 403', function(done) {
+      var user = new User({
+        username: 'impersonator'
+      });
+
+      fetchMockLocal.mock({
+        name: 'impersonator_json',
+        matcher: /\/v1\/users\/impersonator/,
+        method: 'GET',
+        response: {
+          status: 403,
+          body: {
+            message: 'Unauthorized'
+          }
+        }
+      });
+
+      user.fetch()
+        .then(function(result) {
+          expect(result).toBeNull();
+          expect(true).toBeFalsy();
+
+          done();
+        })
+        .catch(function(err) {
+          expect(err.status).toEqual(403);
+          expect(err.statusText).toEqual('Forbidden');
+          expect(err.message).toEqual('Unauthorized');
+          // expect(err.url).toEqual('http://localhost:3000/v1/users/impersonator');
+
+          expect(fetchMockLocal.called('impersonator_json')).toBeTruthy();
+          done();
+        });
+    });
   });
 });
