022bd7acddb2ae8038423e3607df8badafe797c3
cesine <cesine@yahoo.com>
2016-07-03 20:26:16 -0400

    added register

--- components/auth/auth.js
+++ components/auth/auth.js
@@ -86,6 +86,46 @@
         }
       });
     }
+
+    static signup(options) {
+      let xhr;
+      let opts = {
+        method: 'POST'
+      };
+
+      if (!options || !options.username || !options.password) {
+        throw new Error('Login requires username and password');
+      }
+
+      if (options.password !== options.confirmPassword) {
+        throw new Error('Passwords don\'t match, try again.');
+      }
+
+      opts.username = options.username;
+      opts.password = options.password;
+
+      if (options.server) {
+        Auth.server = {
+          url: options.server
+        };
+      }
+
+      return global.fetch(Auth.server.url + '/v1/auth/register', opts).then(function(response) {
+        xhr = response;
+        return response.json();
+      }).then(function(data) {
+        if (xhr.status >= 400) {
+          data.status = xhr.status;
+          data.statusText = xhr.statusText;
+          data.url = xhr.url;
+          throw data;
+        }
+
+        if (data) {
+          return data;
+        }
+      });
+    }
   }
 
   exports.Auth = Auth;
--- test/components/auth/auth-spec.js
+++ test/components/auth/auth-spec.js
@@ -267,4 +267,158 @@ describe('Auth', function() {
         });
     });
   });
+
+  describe('signup', function() {
+    afterEach(function() {
+      fetchMockLocal.restore();
+    });
+
+    it('should require options', function() {
+      var auth = new Auth();
+
+      expect(function() {
+        Auth.signup();
+      }).toThrow(new Error('Login requires username and password'));
+    });
+
+    it('should require username', function() {
+      var auth = new Auth();
+
+      expect(function() {
+        Auth.signup({
+          password: 'IUH@8q98wejnsd'
+        });
+      }).toThrow(new Error('Login requires username and password'));
+    });
+
+    it('should require password', function() {
+      var auth = new Auth();
+
+      expect(function() {
+        Auth.signup({
+          username: 'anonymouse'
+        });
+      }).toThrow(new Error('Login requires username and password'));
+    });
+
+    it('should require a confirm password', function() {
+      var auth = new Auth();
+
+      expect(function() {
+        Auth.signup({
+          username: 'anonymouse',
+          password: 'IUH@8q98wejnsd',
+          confirmPassword: 'IUH@8q98wejndd'
+        });
+      }).toThrow(new Error('Passwords don\'t match, try again.'));
+    });
+
+    it('should signup', function(done) {
+      var auth = new Auth({
+        server: {
+          url: 'http://localhost:3000'
+        }
+      });
+
+      fetchMockLocal.mock({
+        name: 'valid_signup',
+        matcher: /\/v1\/auth\/register/,
+        method: 'POST',
+        response: {
+          id: 1,
+          givenName: 'Anony',
+          language: 'fr',
+          username: 'anonymouse',
+          createdAt: '2016-05-15T15:04:18.027Z',
+          updatedAt: '2016-05-15T15:04:18.027Z'
+        }
+      });
+
+      Auth.signup({
+          username: 'anonymouse',
+          password: 'IUH@8q98wejnsd',
+          confirmPassword: 'IUH@8q98wejnsd'
+        })
+        .then(function(result) {
+          expect(result.givenName).toEqual('Anony');
+          expect(result.language).toEqual('fr');
+          expect(result.createdAt).toEqual('2016-05-15T15:04:18.027Z');
+
+          expect(fetchMockLocal.called('valid_signup')).toBeTruthy();
+
+          done();
+        })
+        .catch(function(err) {
+          expect(err).toBeNull();
+          exect(false).toBeTruthy();
+
+          expect(fetchMockLocal.called('valid_signup')).toBeTruthy();
+          done();
+        });
+    });
+
+    it('should accept a server url', function(done) {
+      var auth = new Auth({
+        server: 'http://example.com'
+      });
+
+      fetchMockLocal.mock({
+        name: 'another_server',
+        matcher: /\/v1\/auth\/register/,
+        method: 'POST',
+        response: {}
+      });
+
+      Auth.signup({
+        username: 'anonymouse',
+        password: 'IUH@8q98wejnsd',
+        confirmPassword: 'IUH@8q98wejnsd',
+        server: 'http://another.com'
+      }).then(function(result) {
+        expect(fetchMockLocal.called('another_server')).toBeTruthy();
+        done();
+      });
+    });
+
+    it('should handle 403', function(done) {
+      var auth = new Auth({
+        server: {
+          url: 'http://localhost:3000'
+        }
+      });
+
+      fetchMockLocal.mock({
+        name: 'invalid_signup',
+        matcher: /\/v1\/auth\/register/,
+        method: 'POST',
+        response: {
+          status: 403,
+          body: {
+            message: 'Username already exists, please try again.'
+          }
+        }
+      });
+
+      Auth.signup({
+          username: 'anonymouse',
+          password: 'IUH@8q98wejnsd',
+          confirmPassword: 'IUH@8q98wejnsd'
+        })
+        .then(function(result) {
+          expect(result).toBeNull();
+          expect(true).toBeFalsy();
+
+          done();
+        })
+        .catch(function(err) {
+          expect(err.status).toEqual(403);
+          expect(err.statusText).toEqual('Forbidden');
+          expect(err.message).toEqual('Username already exists, please try again.');
+          // expect(err.url).toEqual('http://localhost:3000/v1/auth/signup');
+
+          expect(fetchMockLocal.called('invalid_signup')).toBeTruthy();
+          done();
+        });
+    });
+  });
 });
